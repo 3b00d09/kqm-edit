6b3ad341d9d9a9a6321105c704a007ad392b01ed
3b00d09 <bd315977@gmail.com>
2023-07-25 15:41:39 +0100

    installed and configured mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,22 @@
 import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions:[".svx"],
+	layout:{},
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	extensions: ['.svelte', '.svx'],
+	preprocess:[
+		vitePreprocess(), 
+		mdsvex(mdsvexOptions)
+	],
 	optimizeDeps: { include: [ 'objection', 'knex', 'pg' ], exclude: [ 'pg-native' ] },
 
 	kit: {
